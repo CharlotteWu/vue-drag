e8d43a0daf372ad069c80581aba5b8dc92f0ffa1
charlotte <charlottewu1994@gmail.com>
2015-12-25 17:55:03 +0800

    screen

--- scripts/vue-script.js
+++ scripts/vue-script.js
@@ -1,6 +1,41 @@
+ /*Vue.compoment = ('screenDetailEdit',{
+	template:'#screenDetail',
+	props:{
+
+	}
+
+}) */
+
+
 new Vue({
 	el:'#content',
 	data:{
-		several:true, 
+		 selectNum:0, 
+		 screens:[]
+	},
+	methods:{
+		addScreen:function(){
+			var addScreen = this.selectNum; 
+			var nowScreenNum = this.screens.length; 
+			var gap = 0;
+			
+			if(nowScreenNum == 0){
+			  for(var i=0; i<addScreen; i++){
+				this.screens.push(i);
+			  } 
+			}else if(nowScreenNum > addScreen){
+				gap = nowScreenNum - addScreen;
+				for(var i=gap; i>0; i--){
+                  this.screens.pop(i);
+				}
+			}else if(nowScreenNum < addScreen){
+				gap = addScreen - nowScreenNum;
+				for(var i=gap; i<addScreen; i++){
+                  this.screens.push(i);
+				}
+			}
+                       
+
+		},
 	}
 })
