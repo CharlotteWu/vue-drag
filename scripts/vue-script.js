3f7b4c0f83f9f5bac9e42f38e0d96efef6488bb5
charlotte <charlottewu1994@gmail.com>
2015-12-31 18:10:04 +0800

    config

--- scripts/vue-script.js
+++ scripts/vue-script.js
@@ -1,6 +1,8 @@
  
 //data model 
-var screens = [];
+var screens = [
+
+ ];
 //vue component
 //screen component
  
@@ -20,11 +22,102 @@ new Vue({
 		addWord:function(screen){    
 		  screen.push({content:'gaga'});
 		}, 
+		addImg:function(screen,imgsrc){
+			screen.push({img:imgsrc});
+		},
 		removeWord:function(screen,content){
 		   screen.$remove(content);
 		},
 		removeScreen:function(screen){
 		  screens.$remove(screen);
 		} 
+
+
 	}
 })
+
+//var config=
+    {
+        Title:"",
+        BackgroundMusic:
+        {
+          Src:"",
+          IsLoop:false
+          IsShowLogo:true
+          IsAutoStart:true
+        },
+
+        Pages:
+        [
+          {
+            Title:"",
+            IsDisplay:true,
+            BackgroundImage:
+            {
+              Src:""
+            }
+            Elements:
+            [
+              {  //文本元素
+                  Type:1,
+                  ID:"",
+                  Content:"",
+                  IsDisplay:true,
+                  Location:
+                  {
+                    X:10,
+                    Y:20,
+                    Z:100  
+                  }
+                  Animation:
+                  {
+                     Name:"",
+                     Speed:0,
+                     Delay:500
+                  }
+              },
+              {  //图片元素
+                  Type:2,
+                  ID:"",
+                  Src:"",
+                  IsDisplay:true,
+                  Location:
+                  {
+                    X:10,
+                    Y:20,
+                    Z:100  
+                  }
+                  Animation:
+                  {
+                     Name:"",
+                     Speed:0,
+                     Delay:500
+                  }
+              }，
+              {  //图片元素
+                  Type:3,
+                  ID:"",
+                  Src:"",
+                  IsDisplay:true,
+                  Location:
+                  {
+                    X:10,
+                    Y:20,
+                    Z:100  
+                  }
+                  Animation:
+                  {
+                     Name:"",
+                     Speed:0,
+                     Delay:500
+                  }
+              }
+            ]
+
+
+          }
+
+        ]
+
+
+    }
